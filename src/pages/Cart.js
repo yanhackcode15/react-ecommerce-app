e8ada4270a86bfa095f753facb18a351aa7b7751
yanhackcode15 <iamyanhu@gmail.com>
2022-09-11 15:09:25 -0700

    click on delete icon on cart page remove the item from cart

--- src/pages/Cart.js
+++ src/pages/Cart.js
@@ -3,11 +3,16 @@ import {Context} from "../Context"
 {/* <i class="ri-delete-bin-line"></i> */}
 
 export default function Cart() {
-    const {cartedItems} = React.useContext(Context)
+    const {cartedItems, setCartedItems} = React.useContext(Context)
+    function removeFromCart(id){
+        setCartedItems(prev=>{
+            return prev.filter(item=>id!==item.id)
+        })
+    }
     function displayCartedItems () {
         return cartedItems.map(item=>(
             <div className="row" key={item.id}>
-                <i className="ri-delete-bin-line"></i>
+                <i className="ri-delete-bin-line" onClick={()=>removeFromCart(item.id)}></i>
                 <img className="thumbnail" src={item.url}/>
                 <p>$7</p>
             </div>
