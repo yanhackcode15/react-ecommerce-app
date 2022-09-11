ca34ab4f5751ae606a4fbb700d9885231490323b
yanhackcode15 <iamyanhu@gmail.com>
2022-09-11 13:00:06 -0700

    hover

--- src/components/Image.js
+++ src/components/Image.js
@@ -1,14 +1,12 @@
 import React from "react"
 import getClassName from "../utilities/util"
 import {Context} from "../../src/Context"
-//<i class="ri-heart-line"></i>
-//<i class="ri-heart-fill"></i>
-//<i class="ri-shopping-cart-line"></i>
-//<i class="ri-shopping-cart-fill"></i>
+//icon hover
 
 export default function Image({image, index}){
     const {toggleFavorite} = React.useContext(Context)
     const [addedToCart, setAddedToCart] = React.useState(false)
+    const [hover, setHover] = React.useState(false)
     function toggleIcon(fav){
         if (fav) {
             return 'fill'
@@ -17,17 +15,34 @@ export default function Image({image, index}){
             return 'line'
         }
     }
+
     function toggleAddToCart(){
         setAddedToCart(prev=>!prev)
     }
+    
+    function hoverOn(){
+        setHover(true)
+    }
 
+    function hoverOff(){
+        setHover(false)
+    }
 
 
     return (
         <div className={`grid-image ${getClassName(index)}`}>
-            <i className={`heart ri-heart-${toggleIcon(image.isFavorite)}`} onClick={()=>toggleFavorite(image.id)} ></i>
-            <i className={`cart ri-shopping-cart-${toggleIcon(addedToCart)}`} onClick={()=>toggleAddToCart()} ></i>
-            <img className="photo" src={image.url} />
+            {hover&&<i 
+            className={`heart ri-heart-${toggleIcon(image.isFavorite)}`} 
+            onClick={()=>toggleFavorite(image.id)}
+            >
+            </i>}
+            {hover&&<i 
+            className={`cart ri-shopping-cart-${toggleIcon(addedToCart)}`} 
+            onClick={()=>toggleAddToCart()} 
+            >
+            </i>}
+            
+            <img className="photo" src={image.url} onMouseOver={hoverOn} onMouseOut={hoverOff}/>
             
         </div>
     )
