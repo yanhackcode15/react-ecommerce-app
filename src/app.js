a1bde99f03272b5aeaf9c035f295c600b6438988
yanhackcode15 <iamyanhu@gmail.com>
2022-09-12 11:57:51 -0700

    confirmation page

--- src/app.js
+++ src/app.js
@@ -3,6 +3,8 @@ import {BrowserRouter, Route, Routes, Link} from "react-router-dom"
 import Header from "./components/Header"
 import Photos from "./pages/Photos"
 import Cart from "./pages/Cart"
+import Confirmation from "./pages/Confirmation"
+
 //new react router 6 changed how component is nested in the routes
 
 export default function App() {
@@ -13,6 +15,8 @@ export default function App() {
                 <Routes>
                     <Route path="cart" element={<Cart />}></Route>
                     <Route path="/" element={<Photos />}></Route>
+                    <Route path="confirmation" element={<Confirmation />}></Route>
+
                 </Routes>
             </div>
         </BrowserRouter>
--- src/pages/Cart.js
+++ src/pages/Cart.js
@@ -1,18 +1,15 @@
 import React from "react"
+import {Link} from "react-router-dom"
 import {Context} from "../Context"
 import CartItem from "../components/CartItem"
 
+//add a time delay to mimic order placement
 export default function Cart() {
     const {cartedItems, setCartedItems} = React.useContext(Context)
-    
     function placeOrder() {
         setCartedItems([])
     }
 
-
-    function displayOrderStatus(){
-        return null
-    }
     return (
         <main className="cartPage">
             <h1>Checkout</h1>
@@ -21,7 +18,7 @@ export default function Cart() {
         ))}
             </div>
             <h2>{`Total: $${7*cartedItems.length}`}</h2>
-            {cartedItems.length?<button className="order-button" onClick={placeOrder}>Place Order</button>: <h2>Add something to the cart...</h2>}
+            {cartedItems.length?<Link className="place-order" to="/confirmation"><button className="place-order" onClick={placeOrder}>Place Order</button></Link>: <h2>Add something to the cart...</h2>}
         </main>
     )
 }
